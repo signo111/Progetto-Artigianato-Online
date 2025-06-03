8ae31f131c1a9acb9ba259dc32ee2bd47b79ceb6
samuele12345 <samueleriva2016@gmail.com>
2025-06-03 18:45:45 +0200

    fatto primo test di utilizzo di Stripe reativamente al bottone checkout-btn nella pagina del carrello

---
+++ paymentPage.js
@@ -0,0 +1,16 @@
+import ButtonCustomerPortal from '@/components/ButtonCustomerPortal';
+import Pricing from '@/Pricing';
+
+export default function Page() {
+    return (
+        <>
+            <header className="p-4 flex justify-end max-w-7xl mx-auto">
+                <ButtonCustomerPortal />
+            </header>
+
+            <main className="bg-base-200 min-h-screen">
+                <Pricing />
+            </main>
+        </>
+    );
+}
